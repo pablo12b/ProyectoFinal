57906f1d4e81d145d55b37e760db291d2e3195f3
pablo12b <148280248+pablo12b@users.noreply.github.com>
2024-01-17 07:47:36 -0500

    Creacion de medio de ayuda
    se crea menu e inicio

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
